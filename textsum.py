2568c0ea7a5a603a828aee17c06599c45eb742a9
KEERTHANA.R <keerthanaradjendirane@gmail.com>
2024-08-24 15:16:52 +0530

    Add files via upload

---
+++ textsum.py
@@ -0,0 +1,34 @@
+import google.generativeai as genai
+import streamlit as st
+from langchain import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+
+
+api_key = 'AIzaSyDX3HE-dhk-0xUc7amKaIz8avJ6gpUFeGo'
+genai.configure(api_key=api_key)
+
+
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+
+def summarize_text(text, max_words):
+    prompt = f"Summarize the following text in {max_words} words: {text}"
+    response = model.generate_content([prompt])
+    return response.text
+
+
+st.title("Text Summarization Tool")
+st.write("Enter the text you want to summarize and click the button to get a summary.")
+
+user_text = st.text_area("Enter your text here:", height=300)
+
+
+max_words = st.slider("Select the number of words for the summary:", min_value=10, max_value=2000, value=100, step=10)
+
+if st.button("Summarize Text"):
+    if user_text:
+        summary = summarize_text(user_text, max_words)
+        st.write("Summary:")
+        st.write(summary)
+    else:
+        st.write("Please enter some text to summarize.")
